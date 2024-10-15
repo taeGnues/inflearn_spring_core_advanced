9181b158860b6690f328be5aba6ccfd949cdc590
tsjeong <tsjeong@suresofttech.com>
2024-10-16 07:53:13 +0900

    ThreadLocal - 개발 & 적용

--- src/main/java/com/kimyeonghan/advanced/LogTraceConfig.java
+++ src/main/java/com/kimyeonghan/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.kimyeonghan.advanced;
 
-import com.kimyeonghan.advanced.trace.logtrace.FieldLogTrace;
 import com.kimyeonghan.advanced.trace.logtrace.LogTrace;
+import com.kimyeonghan.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace(); // 싱글톤으로 빈 등록. => JVM에 딱 1개 있는 인스턴스라는 의미임. 즉, traceIdHolder에 여러 쓰레드가 동시에 접근하여 문제 발생.
+        return new ThreadLocalLogTrace(); // 싱글톤으로 빈 등록. => JVM에 딱 1개 있는 인스턴스라는 의미임. 즉, traceIdHolder에 여러 쓰레드가 동시에 접근하여 문제 발생.
     }
 }
