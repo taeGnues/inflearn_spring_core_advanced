5aa4ce08bb2cf1b2b6f60abd35298a283eddd141
tsjeong <tsjeong@suresofttech.com>
2024-10-16 10:16:14 +0900

    전략 패턴 - 익명 클래스 사용

--- src/test/java/com/kimyeonghan/advanced/trace/strategy/ContextV0Test.java
+++ src/test/java/com/kimyeonghan/advanced/trace/strategy/ContextV0Test.java
@@ -1,6 +1,7 @@
 package com.kimyeonghan.advanced.trace.strategy;
 
 import com.kimyeonghan.advanced.trace.strategy.code.strategy.ContextV1;
+import com.kimyeonghan.advanced.trace.strategy.code.strategy.Strategy;
 import com.kimyeonghan.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import com.kimyeonghan.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
@@ -54,5 +55,28 @@ public class ContextV0Test {
         contextV2.execute();
     }
 
+    /**
+     * 전략 패턴 사용 (익명 클래스)
+     */
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비지니스 로직 1 실행");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비지니스 로직 2 실행"));
+        // 익명 내부 클래스를 람다로 짧게 작성 가능.
+        // 단, 인터페이스에 단 1개의 메소드만 존재해야함.
+        contextV2.execute();
+
+
+    }
 
 }
